5903219e50762b9a7f1e1f0a152552a3aaeb4c7b
castlechoi <castlechoi@naver.com>
2023-05-16 21:14:55 +0900

    upload NBEATS model in paper

--- TimeSeries/nbeats.py
+++ TimeSeries/nbeats.py
@@ -3,33 +3,36 @@ import torch.nn as nn
 import torch.optim as optim
 
 class Block(nn.Module):
-    def __init__(self, config,blockType):
+    def __init__(self, config,loopback,blockType):
         super(Block,self).__init__()
         self.inputSize = config.window_size
         self.predictSize = config.horizon
-        self.theta = config.theta
+        self.theta = 32
         self.blockType = blockType
+        self.loopback = loopback * 6
 
         self.fcStack = nn.Sequential(
-            nn.Linear(self.inputSize, 256),
+            nn.Linear(self.loopback, 512),
             nn.ReLU(),
-            nn.Linear(256, 256),
+            nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(256, 256),
+            nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(256, 256),
+            nn.Linear(512, 512),
             nn.ReLU()
         )
         self.genericBackCastFC = nn.Sequential(
-            nn.Linear(256, self.theta),
-            nn.Linear(self.theta, self.inputSize)
+            nn.Linear(512, self.theta),
+            nn.ReLU(),
+            nn.Linear(self.theta, self.loopback)
         )
         self.genericForeCastFC = nn.Sequential(
-            nn.Linear(256, self.theta),
+            nn.Linear(512, self.theta),
+            nn.ReLU(),
             nn.Linear(self.theta, self.predictSize)
         )
 
-    def forward(self,x):  # x.shape = torch.Size ( batchSize , observation)
+    def forward(self,x):# x.shape = torch.Size ( batchSize , observation)
         out = self.fcStack(x) # ( batchSize * 256 )
 
         backcast = self.genericBackCastFC(out)
@@ -37,40 +40,25 @@ class Block(nn.Module):
         return backcast, forecast
 
 class Stack(nn.Module):
-    def __init__(self, config, blockType, device):
+    def __init__(self, config,loopback, blockType, device):
         super(Stack,self).__init__()
         self.device = device
         self.config = config
+        self.loopback = loopback
         #  depth 5 stack
-        self.block1 = Block(config,'generic').to(device)
-        self.block2 = Block(config,'generic').to(device)
-        self.block3 = Block(config,'generic').to(device)
-        self.block4 = Block(config,'generic').to(device)
-        self.block5 = Block(config,'generic').to(device)
+        self.block = nn.ModuleList([Block(config,loopback,'generic').to(device) for i in range(30)])
+        
 
     def forward(self,x):
-        back = torch.zeros(x.size(0),x.size(1)).to(self.device)
+        back = torch.zeros(x.size(0), x.size(1)).to(self.device)
         fore = torch.zeros(x.size(0), self.config.horizon).to(self.device)
+        back = x
 
-        outBack, outFore = self.block1(x)
-        back = x - outBack
-        fore += outFore
+        for i in range(30):
+            outBack, outFore = self.block[i](back)
+            back = back - outBack
+            fore = outFore + fore
 
-        outBack, outFore = self.block2(x)
-        back -= outBack
-        fore += outFore
-
-        outBack, outFore = self.block3(x)
-        back -= outBack
-        fore += outFore
-
-        outBack, outFore = self.block4(x)
-        back -= outBack
-        fore += outFore
-
-        outBack, outFore = self.block5(x)
-        back -= outBack
-        fore += outFore
         return back, fore
 
 class NBEATS(nn.Module):
@@ -79,16 +67,25 @@ class NBEATS(nn.Module):
         #  depth 5 stack
         self.device = device
         self.config = config
-        self.stack1 = Stack(config,'generic',device).to(device)
-
+        self.stack2 = Stack(config,2,'generic',device).to(device)
+        self.stack3 = Stack(config,3,'generic',device).to(device)
+        self.stack4 = Stack(config,4,'generic',device).to(device)
+        self.stack5 = Stack(config,5,'generic',device).to(device)
+        self.stack6 = Stack(config,6,'generic',device).to(device)
+        self.stack7 = Stack(config,7,'generic',device).to(device)
+        
 
     def forward(self,x):
         x = x.view(x.size(0), -1)
-        back = torch.zeros(x.size(0),x.size(1)).to(self.device)
-        fore = torch.zeros(x.size(0),self.config.horizon).to(self.device)
-
-        outBack, outFore = self.stack1(x)
-
-        back = x - outBack
-        fore += outFore
-        return fore
+        _, fore2 = self.stack2(x[:,30:]) 
+        
+        _, fore3 = self.stack3(x[:,24:])
+        
+        _, fore4 = self.stack4(x[:,18:])
+        
+        _, fore5 = self.stack5(x[:,12:])
+        
+        _, fore6 = self.stack6(x[:,6:])
+        
+        _, fore7 = self.stack7(x)
+        return (fore2 + fore3 + fore4 + fore5 + fore6 + fore7) / 6
